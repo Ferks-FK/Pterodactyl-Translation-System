04845c6088cb45d5e1a3de6d17f97cb2a62f4f2c
Ferks <69549678+Ferks-FK@users.noreply.github.com>
2022-01-19 23:58:14 -0400

    Updated to work with the latest version of panel

---
+++ Languages ready/index.js
@@ -0,0 +1,20 @@
+import PT from './Portuguese';
+import EN from './English';
+import ES from './Spanish';
+import RU from './Russian';
+import FR from './French';
+import DE from './German';
+/*
+
+Import the language file here, example: 
+
+import MyLanguageIsRussian from './Russian';
+
+Now export your file into the function below, example:
+
+export { PT, EN, ES, MyLanguageIsRussian }
+
+Remember to use the same names as the import names.
+
+*/
+export { PT, EN, ES, RU, FR, DE }
